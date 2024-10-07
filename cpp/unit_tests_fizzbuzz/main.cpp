55138f74ab4a2f2360871ce26dd1cfd2b7009756
VVlktor <student@student.machine>
2024-10-07 15:05:27 +0000

    studentmachine send

--- cpp/unit_tests_fizzbuzz/main.cpp
+++ cpp/unit_tests_fizzbuzz/main.cpp
@@ -6,10 +6,13 @@ using namespace std;
 int main() {
     cout << "Podana liczba 3:" << endl;
     cout << FizzBuzz(3) << endl;
+
     cout << "Podana liczba 20:" << endl;
     cout << FizzBuzz(20) << endl;
+
     cout << "Podana liczba 30:" << endl;
     cout << FizzBuzz(30) << endl;
+
     return 0;
 }
 
