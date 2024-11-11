b3ca8ca1b95606769f163f0a591d0244244781ed
VVlktor <125738563+VVlktor@users.noreply.github.com>
2024-11-11 17:49:02 +0100

    Update main_test.cpp

--- cpp/SchodyZTestami/main_test.cpp
+++ cpp/SchodyZTestami/main_test.cpp
@@ -1,13 +1,27 @@
 // main_test.cpp
 #include <gtest/gtest.h>
 #include "funkcje.cpp"
-
+#include "SchodyClass.cpp"
 
 TEST(mojagrupatestow, test1) {
     double wysokoscKlatki = 320;
-	double szerokoscKlatki = 590;
-    string wynik = WyliczSchody(wysokoscKlatki, szerokoscKlatki);
-    EXPECT_EQ(wynik, "");
+    double szerokoscKlatki = 590;
+    Schody wynik = ObliczSchody(wysokoscKlatki, szerokoscKlatki);
+    EXPECT_EQ(wynik.liczbaSchodow, 17);
+}
+
+TEST(mojagrupatestow, test2) {
+    double wysokoscKlatki = 700;
+    double szerokoscKlatki = 1300;
+    Schody wynik = ObliczSchody(wysokoscKlatki, szerokoscKlatki);
+    EXPECT_EQ(wynik.liczbaSchodow, 37);
+}
+
+TEST(mojagrupatestow, test3) {
+    double wysokoscKlatki = 213;
+    double szerokoscKlatki = 511;
+    Schody wynik = ObliczSchody(wysokoscKlatki, szerokoscKlatki);
+    EXPECT_EQ(wynik.liczbaSchodow, 12);
 }
 
 int main(int argc, char **argv) {
