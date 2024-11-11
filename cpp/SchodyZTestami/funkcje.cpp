790c37634edbaa16f8332385baa9d22aa6f8f114
VVlktor <125738563+VVlktor@users.noreply.github.com>
2024-11-11 17:45:06 +0100

    Update funkcje.cpp

--- cpp/SchodyZTestami/funkcje.cpp
+++ cpp/SchodyZTestami/funkcje.cpp
@@ -12,7 +12,7 @@ Schody ObliczSchody(double H_klatki, double L_klatki) {
     for (int liczbaStopni = ceil(H_klatki / max_H_Stopnia); liczbaStopni <= floor(H_klatki / min_H_Stopnia); liczbaStopni++) {
         double wysokoscStopnia = H_klatki / liczbaStopni;
         if (!(wysokoscStopnia < min_H_Stopnia || wysokoscStopnia > max_H_Stopnia)) {
-            for (double szerokoscStopnia = min_L_Stopnia; szerokoscStopnia <= max_L_Stopnia; szerokoscStopnia += 0.5) {
+            for (double szerokoscStopnia = min_L_Stopnia; szerokoscStopnia <= max_L_Stopnia; szerokoscStopnia += 0.1) {
                 if (szerokoscStopnia * liczbaStopni <= L_klatki) {
                     schody.liczbaSchodow = liczbaStopni;
                     schody.wysokoscSchoda = wysokoscStopnia;
