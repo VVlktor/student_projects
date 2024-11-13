b1916b52372a5d320ba0ad9ff564b2689ac323ed
VVlktor <125738563+VVlktor@users.noreply.github.com>
2024-11-13 09:39:16 +0100

    Update main_test.cpp

--- cpp/SchodyZTestami/main_test.cpp
+++ cpp/SchodyZTestami/main_test.cpp
@@ -3,25 +3,46 @@
 #include "funkcje.cpp"
 #include "SchodyClass.cpp"
 
-TEST(mojagrupatestow, test1) {
+TEST(testyLiczby, test1) {
     double wysokoscKlatki = 320;
     double szerokoscKlatki = 590;
     Schody wynik = ObliczSchody(wysokoscKlatki, szerokoscKlatki);
     EXPECT_EQ(wynik.liczbaSchodow, 17);
 }
 
-TEST(mojagrupatestow, test2) {
+TEST(testyLiczby, test2) {
     double wysokoscKlatki = 700;
     double szerokoscKlatki = 1300;
     Schody wynik = ObliczSchody(wysokoscKlatki, szerokoscKlatki);
     EXPECT_EQ(wynik.liczbaSchodow, 37);
 }
 
-TEST(mojagrupatestow, test3) {
+TEST(testyLiczby, test3) {
     double wysokoscKlatki = 213;
     double szerokoscKlatki = 511;
     Schody wynik = ObliczSchody(wysokoscKlatki, szerokoscKlatki);
     EXPECT_EQ(wynik.liczbaSchodow, 12);
+}
+
+TEST(testyWysokosci, test4){
+    double wysokoscKlatki = 380;
+    double szerokoscKlatki = 590;
+    Schody wynik = ObliczSchody(wysokoscKlatki, szerokoscKlatki);
+    EXPECT_EQ(wynik.wysokoscSchoda, 19);
+}
+
+TEST(testyWysokosci, test5){
+    double wysokoscKlatki = 750;
+    double szerokoscKlatki = 1300;
+    Schody wynik = ObliczSchody(wysokoscKlatki, szerokoscKlatki);
+    EXPECT_EQ(wynik.wysokoscSchoda, 18.75);
+}
+
+TEST(testyWysokosci, test6){
+    double wysokoscKlatki = 210;
+    double szerokoscKlatki = 400;
+    Schody wynik = ObliczSchody(wysokoscKlatki, szerokoscKlatki);
+    EXPECT_EQ(wynik.wysokoscSchoda, 17.5);
 }
 
 int main(int argc, char **argv) {
