85a3d095c876601ae87dfe8313d30412d363ac5b
VVlktor <125738563+VVlktor@users.noreply.github.com>
2024-11-25 17:17:06 +0100

    Update SchodyClass.cpp

--- cpp/SchodyZTestami/SchodyClass.cpp
+++ cpp/SchodyZTestami/SchodyClass.cpp
@@ -14,14 +14,25 @@ public:
         czyWyliczone = false;
     }
 
-    void Wypisz() {
-        if (czyWyliczone) {
-            cout << "Liczba stopni: " << liczbaSchodow << endl;
-            cout << "Wysokosc stopnia: " << wysokoscSchoda  << endl;
-            cout << "Szerokosc stopnia: " << szerokoscSchoda << endl << endl;
+    Schody ObliczSchody(double H_klatki, double L_klatki) {
+        Schody schody = Schody();
+        double min_H_Stopnia = 15.0;
+        double max_H_Stopnia = 19.0;
+        double min_L_Stopnia = 29.0;
+        double max_L_Stopnia = 32.0;
+        for (int liczbaStopni = ceil(H_klatki / max_H_Stopnia); liczbaStopni <= floor(H_klatki / min_H_Stopnia); liczbaStopni++) {
+            double wysokoscStopnia = H_klatki / liczbaStopni;
+            if (!(wysokoscStopnia < min_H_Stopnia || wysokoscStopnia > max_H_Stopnia)) {
+                for (double szerokoscStopnia = min_L_Stopnia; szerokoscStopnia <= max_L_Stopnia; szerokoscStopnia += 0.1) {
+                    if (szerokoscStopnia * liczbaStopni <= L_klatki) {
+                        schody.liczbaSchodow = liczbaStopni;
+                        schody.wysokoscSchoda = wysokoscStopnia;
+                        schody.szerokoscSchoda = szerokoscStopnia;
+                        schody.czyWyliczone = true;
+                    }
+                }
+            }
         }
-        else {
-            cout << "Nie udalo sie wyliczyc schodow" << endl;
-        }
+        return schody;
     }
 };
