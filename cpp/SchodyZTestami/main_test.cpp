39c6278e884bd5fc85b1816422ce501fcc943423
VVlktor <125738563+VVlktor@users.noreply.github.com>
2024-11-27 09:19:47 +0100

    Update main_test.cpp

--- cpp/SchodyZTestami/main_test.cpp
+++ cpp/SchodyZTestami/main_test.cpp
@@ -6,7 +6,8 @@
 TEST(testyLiczb, test1) {
     double wysokoscKlatki = 320;
     double szerokoscKlatki = 580;
-    Schody wynik = ObliczSchody(wysokoscKlatki, szerokoscKlatki);
+    Staris wynik = Staris();
+    wynik = wynik.ObliczSchody(wysokoscKlatki, szerokoscKlatki);
     EXPECT_EQ(wynik.liczbaSchodow, 20);
     EXPECT_EQ(wynik.wysokoscSchoda, 16);
     EXPECT_EQ(wynik.szerokoscSchoda, 29);
@@ -15,7 +16,8 @@ TEST(testyLiczb, test1) {
 TEST(testyLiczb, test2) {
     double wysokoscKlatki = 700;
     double szerokoscKlatki = 1300;
-    Schody wynik = ObliczSchody(wysokoscKlatki, szerokoscKlatki);
+    Staris wynik = Staris();
+    wynik = wynik.ObliczSchody(wysokoscKlatki, szerokoscKlatki);
     EXPECT_EQ(wynik.liczbaSchodow, 44);
     EXPECT_EQ(wynik.wysokoscSchoda, 15.91);
     EXPECT_EQ(wynik.szerokoscSchoda, 29.50);
@@ -24,7 +26,8 @@ TEST(testyLiczb, test2) {
 TEST(testyLiczb, test3) {
     double wysokoscKlatki = 210;
     double szerokoscKlatki = 407;
-    Schody wynik = ObliczSchody(wysokoscKlatki, szerokoscKlatki);
+    Staris wynik = Staris();
+    wynik = wynik.ObliczSchody(wysokoscKlatki, szerokoscKlatki);
     EXPECT_EQ(wynik.liczbaSchodow, 14);
     EXPECT_EQ(wynik.wysokoscSchoda, 15);
     EXPECT_EQ(wynik.szerokoscSchoda, 29);
@@ -33,21 +36,24 @@ TEST(testyLiczb, test3) {
 TEST(testyWykonywalnosci, test4){
     double wysokoscKlatki = 180;
     double szerokoscKlatki = 200;
-    Schody wynik = ObliczSchody(wysokoscKlatki, szerokoscKlatki);
+    Staris wynik = Staris();
+    wynik = wynik.ObliczSchody(wysokoscKlatki, szerokoscKlatki);
     EXPECT_EQ(wynik.czyWyliczone, false);
 }
 
 TEST(testyWykonywalnosci, test5){
     double wysokoscKlatki = 750;
     double szerokoscKlatki = 1300;
-    Schody wynik = ObliczSchody(wysokoscKlatki, szerokoscKlatki);
+    Staris wynik = Staris();
+    wynik = wynik.ObliczSchody(wysokoscKlatki, szerokoscKlatki);
     EXPECT_EQ(wynik.czyWyliczone, true);
 }
 
 TEST(testyWykonywalnosci, test6){
     double wysokoscKlatki = 210;
     double szerokoscKlatki = 400;
-    Schody wynik = ObliczSchody(wysokoscKlatki, szerokoscKlatki);
+    Staris wynik = Staris();
+    wynik = wynik.ObliczSchody(wysokoscKlatki, szerokoscKlatki);
     EXPECT_EQ(wynik.czyWyliczone, true);
 }
 
